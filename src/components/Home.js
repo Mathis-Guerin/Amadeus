1216e2281cc21f5e10764b3cd5e4884491ba8182
mathisGuerin <mathis.guerin@idvroom.com>
2019-02-13 15:00:39 +0100

    Create Home page

---
+++ src/components/Home.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import HomeVideoWebm from '../assets/img/HomeVideo2.webm'
+import HomeVideoMp4 from '../assets/img/HomeVideo2.mp4'
+import HomeVideoJpg from '../assets/img/HomeVideo2.jpg'
+import '../styles/Home.scss'
+
+
+function Home() {
+    return (
+        <div className="Home">
+            <video autoPlay loop>
+                <source src={HomeVideoWebm} type="video/webm"/>
+                <source src={HomeVideoMp4} type="video/mp4"/>
+            </video>
+            <div className="Home-title"> 
+                Discover the world
+            </div>
+            <div className="Home-content"> 
+                Extra content
+            </div>
+        </div>
+    )
+}
+
+export default Home;
