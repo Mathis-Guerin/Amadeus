e3f3acf528d935e769b901685e2f8c58a8ff3ce1
mathisGuerin <mathis.guerin@idvroom.com>
2019-02-15 16:09:03 +0100

    Create new component : Flight details

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,3 +1,13 @@
-export default function capitalizeFirstLetter(string) {
+export function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
+
+export default function formatDate(param) {
+    const date = new Date(param)
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+
+    return {
+        date: date.toLocaleDateString("en-US", options),
+        time: date.toLocaleTimeString("en-US")
+    }
 }
